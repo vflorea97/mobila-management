24043f5ae843a87187c692cc1de9d235b1f2fd00
Valentin <vflorea97@yahoo.com>
2023-01-25 21:25:46 +0200

    upload

---
+++ src/main/java/ro/mycode/mobilamanagement/MobilaManagementApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.mobilamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilaManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilaManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/mobilamanagement/MobilaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.mobilamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilaManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
